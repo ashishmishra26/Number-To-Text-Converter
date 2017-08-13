6ffa81d2353d6815be0fef847ca4991a8c7ed849
Ashish Mishra <b.s.ashish26@gmail.com>
2017-08-13 19:45:43 +0530

    test file error fixed

--- test/identifier.test.js
+++ test/identifier.test.js
@@ -4,21 +4,15 @@ import {
 import {
     Identifier
 } from "../src/identifier.js";
-
 test("Identifier", () => {
     let obj = new NumberToWord();
     expect((obj.number_to_word(1)).trim()).toBe("one");
-
 });
-
 test("identifier", () => {
     let obj = new NumberToWord();
     expect((obj.number_to_word(0)).trim()).toBe("");
-
 });
-
 test("identfier", () => {
     let obj = new Identifier("hi 100");
     expect(obj.identify()).toBe(undefined);
-
 });
--- test/money.test.js
+++ test/money.test.js
@@ -1,10 +1,10 @@
-import {Money} from '../src/money.js';
+import {Money} from "../src/money.js";
 import {NumberToWord} from "../src/numberword.js";
-test('Money', () => {
+test("Money", () => {
     let obj=new Money(null,"9,000");
     expect(obj.output()).toBe("nine  thousand ");
-})
-test('Money', () => {
+});
+test("Money", () => {
     let obj=new Money(null,"8,000,000");
     expect(obj.convert()).toBe(undefined);
-})
+});
