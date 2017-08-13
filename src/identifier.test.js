8b60999736a112cd7071356a5a20a13d317c43e1
ashishmishra26 <b.s.ashish26@gmail.com>
2017-08-13 23:23:02 +0530

    Delete identifier.test.js

--- src/identifier.test.js
+++
@@ -1,9 +0,0 @@
-import {Identifier} from './identifier.js'
-
-test('Dog.bark', () => {
-    let input = "hi 100";
-    const obj = new Identifier(input);
-    obj.identify();
-    jest.fn()
-    expect(obj.decide).toHaveBeenCalled();
-})
