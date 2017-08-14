1cf9d5ca62cb71e43e8654e2cbd51e9a9b095ea7
Ashish Mishra <b.s.ashish26@gmail.com>
2017-08-14 14:10:55 +0530

    css added

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
-const path = require('path');
-var webpack = require('webpack');
+const path = require("path");
+var webpack = require("webpack");
 
 module.exports = {
-  entry: './src/app.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build')
-  },
-  
-  module: {
-    loaders: [{
-        test: /(\.js)$/,
-        loader: 'babel-loader',
-        query: {
-          presets: 'es2015'
+    entry: "./src/app.js",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "build")
+    },
+
+    module: {
+        loaders: [{
+            test: /(\.js)$/,
+            loader: "babel-loader",
+            query: {
+                presets: "es2015"
+            },
+
+            exclude: /(node_modules)/
         },
 
-        exclude: /(node_modules)/
-      },
-
-    ]
-  }
+        ]
+    }
 };
